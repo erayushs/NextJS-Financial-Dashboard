6249736eac54df4fc705a534617b4cfeea560ce6
Ayush Singh <anmol8960@gmail.com>
2025-08-15 09:33:53 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="h-[70px] bg-[#092e64]">
+      <div className="bg-yellow-100 w-[50px] h-[50px] pt-4"></div>
+    </div>
+  );
+};
+
+export default Navbar;
